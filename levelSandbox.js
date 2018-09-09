6e9897a08051024a3336730523b1cf8cdbbb821a
Gonzalo Rodriguez <gonzalo@rodriguez-blanco.com>
2018-09-09 16:15:18 +0200

    style: formatting

--- levelSandbox.js
+++ levelSandbox.js
@@ -7,31 +7,31 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
-  })
+function addLevelDBData(key, value) {
+    db.put(key, value, function (err) {
+        if (err) return console.log('Block ' + key + ' submission failed', err);
+    })
 }
 
 // Get data from levelDB with key
-function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
+function getLevelDBData(key) {
+    db.get(key, function (err, value) {
+        if (err) return console.log('Not found!', err);
+        console.log('Value = ' + value);
+    })
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
     let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+    db.createReadStream().on('data', function (data) {
+        i++;
+    }).on('error', function (err) {
+        return console.log('Unable to read data stream!', err)
+    }).on('close', function () {
+        console.log('Block #' + i);
+        addLevelDBData(i, value);
+    });
 }
 
 /* ===== Testing ==============================================================|
@@ -46,9 +46,9 @@ function addDataToLevelDB(value) {
 |  ===========================================================================*/
 
 
-(function theLoop (i) {
-  setTimeout(function () {
-    addDataToLevelDB('Testing data');
-    if (--i) theLoop(i);
-  }, 100);
+(function theLoop(i) {
+    setTimeout(function () {
+        addDataToLevelDB('Testing data');
+        if (--i) theLoop(i);
+    }, 100);
 })(10);
